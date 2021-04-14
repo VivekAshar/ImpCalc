4cb7596c23d48d81e20f42244f0166f1255c7dad
Adithyya Vamsi <cskadi1999@gmail.com>
2021-04-14 17:50:16 +0530

    added include

---
+++ 3_Implementation/inc/impedance.h
@@ -0,0 +1,17 @@
+
+#ifndef __impedance_h__
+#define __impedance_h__
+
+#include<stdio.h>
+//#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+#define pi 3.142
+
+int ripcs(int mmip,int smip);
+int riscp(int mmip,int smip);
+int RLCS ();
+int RLCP ();
+int resfreq();
+
+#endif
