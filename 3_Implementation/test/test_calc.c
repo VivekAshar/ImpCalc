377fae8eb3832fb5f81cadc59b3147ed1d6a3cc8
Adithyya Vamsi <cskadi1999@gmail.com>
2021-04-15 10:12:42 +0530

    update test_calc

--- 3_Implementation/test/test_calc.c
+++ 3_Implementation/test/test_calc.c
@@ -2,7 +2,41 @@
 #include "unity.h"
 
 #include<stdlib.h>
+void test_rs(void);
+void test_rp(void);
+void test_cs(void);
+void test_cp(void);
+void test_is(void);
+void test_ip(void);
+void test_rlcs(void);
+void test_rlcp(void);
+void test_rf(void);
 
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_rs);
+  RUN_TEST(test_rp);
+  RUN_TEST(test_cp);
+  RUN_TEST(test_cs);
+  RUN_TEST(test_is);
+  RUN_TEST(test_ip);
+  RUN_TEST(test_rlcs);
+  RUN_TEST(test_rlcp);
+  RUN_TEST(test_rf);
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
 int a[5]={10,20,30,40,50};
 
 void test_rs(void)
@@ -35,34 +69,9 @@ void test_rcls(void)
 }
 void test_rclp(void)
 {
-    TEST_ASSERT_EQUAL(0.0016,RLCp(10,10,10,10));
+    TEST_ASSERT_EQUAL(0.0016,RLCP(10,10,10,10));
 }
 void test_rf(void)
 {
     TEST_ASSERT_EQUAL(0.015913,resfreq(10,10));
-}
-
-/* Required by the unity test framework */
-void setUp(){}
-/* Required by the unity test framework */
-void tearDown(){}
-
-/* Start of the application test */
-int main()
-{
-/* Initiate the Unity Test Framework */
-  UNITY_BEGIN();
-
-/* Run Test functions */
-  RUN_TEST(test_rs);
-  RUN_TEST(test_rp);
-  RUN_TEST(test_cp);
-  RUN_TEST(test_cs);
-  RUN_TEST(test_is);
-  RUN_TEST(test_ip);
-  RUN_TEST(test_rlcs);
-  RUN_TEST(test_rlcp);
-  RUN_TEST(test_rf);
-  /* Close the Unity Test Framework */
-  return UNITY_END();
 }
