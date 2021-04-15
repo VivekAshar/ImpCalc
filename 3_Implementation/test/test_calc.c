27a1765264d29919d7ceb7a74c7b2af98bdebea4
Adithyya Vamsi <cskadi1999@gmail.com>
2021-04-15 10:06:10 +0530

    update test files

--- 3_Implementation/test/test_calc.c
+++ 3_Implementation/test/test_calc.c
@@ -1,202 +1,68 @@
-#include "complex_calc.h"
+#include "impedance.h"
 #include "unity.h"
 
-static complex_t c1 = {0, 0};
-static complex_t c2 = {0, 0};
-static complex_t c3 = {0, 0};
-static complex_t c4 = {0, 0};
-static complex_t result = {0, 0};
+#include<stdlib.h>
+
+int a[5]={10,20,30,40,50};
+
+void test_rs(void)
+{
+    TEST_ASSERT_EQUAL(20,riscp(5,a));
+}
+void test_rp(void)
+{
+    TEST_ASSERT_EQUAL(4.38,ripcs(5,a));
+}
+void test_cs(void)
+{
+    TEST_ASSERT_EQUAL(4.38,ripcs(5,a));
+}
+void test_cp(void)
+{
+    TEST_ASSERT_EQUAL(20,riscp(5,a));
+}
+void test_is(void)
+{
+    TEST_ASSERT_EQUAL(20,riscp(5,a));
+}
+void test_ip(void)
+{
+    TEST_ASSERT_EQUAL(4.38,ripcs(5,a));
+}
+void test_rcls(void)
+{
+    TEST_ASSERT_EQUAL(628.3965,RLCS(10,10,10,10));
+}
+void test_rclp(void)
+{
+    TEST_ASSERT_EQUAL(0.0016,RLCp(10,10,10,10));
+}
+void test_rf(void)
+{
+    TEST_ASSERT_EQUAL(0.015913,resfreq(10,10));
+}
+
 /* Required by the unity test framework */
-void setUp()
-{
-}
+void setUp(){}
 /* Required by the unity test framework */
-void tearDown()
+void tearDown(){}
+
+/* Start of the application test */
+int main()
 {
-}
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
 
-void c_add(void){
-    c1.real = 0;
-    c1.imaginary = 0;
-    
-    c2.real = 0;
-    c2.imaginary = 0;
-    TEST_ASSERT_EQUAL(1, complex_add(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(0, result.real);
-    TEST_ASSERT_EQUAL(0, result.imaginary);
-
-    c1.real = 25;
-    c1.imaginary = 15;
-    
-    c2.real = 75;
-    c2.imaginary = 95;
-    TEST_ASSERT_EQUAL(1, complex_add(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(100, result.real);
-    TEST_ASSERT_EQUAL(110, result.imaginary);
-
-    c1.real = -25;
-    c1.imaginary = -15;
-    
-    c2.real = 75;
-    c2.imaginary = 95;
-    TEST_ASSERT_EQUAL(1, complex_add(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(50, result.real);
-    TEST_ASSERT_EQUAL(80, result.imaginary);
-
-    c1.real = -25;
-    c1.imaginary = -15;
-    
-    c2.real = -75;
-    c2.imaginary = -95;
-    TEST_ASSERT_EQUAL(1, complex_add(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(-100, result.real);
-    TEST_ASSERT_EQUAL(-110, result.imaginary);
-
-    c1.real = 2;
-    c1.imaginary = 0;
-    
-    c2.real = -5;
-    c2.imaginary = 0;
-    TEST_ASSERT_EQUAL(1, complex_add(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(-3, result.real);
-    TEST_ASSERT_EQUAL(0, result.imaginary);
-
-    c1.real = 0;
-    c1.imaginary = 10;
-    
-    c2.real = 0;
-    c2.imaginary = 5;
-    TEST_ASSERT_EQUAL(1, complex_add(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(0, result.real);
-    TEST_ASSERT_EQUAL(15, result.imaginary);
-}
-
-void c_sub(void){
-    c1.real = 0;
-    c1.imaginary = 0;
-    
-    c2.real = 0;
-    c2.imaginary = 0;
-    TEST_ASSERT_EQUAL(1, complex_sub(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(0, result.real);
-    TEST_ASSERT_EQUAL(0, result.imaginary);        
-
-    c1.real = 25;
-    c1.imaginary = 15;
-    
-    c2.real = 75;
-    c2.imaginary = 95;
-    TEST_ASSERT_EQUAL(1, complex_sub(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(-50, result.real);
-    TEST_ASSERT_EQUAL(-80, result.imaginary);
-
-    c1.real = -25;
-    c1.imaginary = -15;
-    
-    c2.real = 75;
-    c2.imaginary = 95;
-    TEST_ASSERT_EQUAL(1, complex_sub(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(-100, result.real);
-    TEST_ASSERT_EQUAL(-110, result.imaginary);
-
-    c1.real = -25;
-    c1.imaginary = -15;
-    
-    c2.real = -75;
-    c2.imaginary = -95;
-    TEST_ASSERT_EQUAL(1, complex_sub(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(50, result.real);
-    TEST_ASSERT_EQUAL(80, result.imaginary);
-
-    c1.real = 2;
-    c1.imaginary = 0;
-    
-    c2.real = -5;
-    c2.imaginary = 0;
-    TEST_ASSERT_EQUAL(1, complex_sub(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(7, result.real);
-    TEST_ASSERT_EQUAL(0, result.imaginary);
-
-    c1.real = 0;
-    c1.imaginary = 10;
-    
-    c2.real = 0;
-    c2.imaginary = 5;
-    TEST_ASSERT_EQUAL(1, complex_sub(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(0, result.real);
-    TEST_ASSERT_EQUAL(5, result.imaginary);
-}
-
-void c_mul(void){
-    c1.real = 0;
-    c1.imaginary = 0;
-    
-    c2.real = 0;
-    c2.imaginary = 0;
-    TEST_ASSERT_EQUAL(1, complex_mul(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(0, result.real);
-    TEST_ASSERT_EQUAL(0, result.imaginary);
-
-    c1.real = 25;
-    c1.imaginary = 15;
-    
-    c2.real = 75;
-    c2.imaginary = 95;
-    TEST_ASSERT_EQUAL(1, complex_mul(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(450, result.real);
-    TEST_ASSERT_EQUAL(3500, result.imaginary);
-
-    c1.real = -25;
-    c1.imaginary = -15;
-    
-    c2.real = 75;
-    c2.imaginary = 95;
-    TEST_ASSERT_EQUAL(1, complex_mul(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(-450, result.real);
-    TEST_ASSERT_EQUAL(-3500, result.imaginary);
-
-    c1.real = -25;
-    c1.imaginary = -15;
-    
-    c2.real = -75;
-    c2.imaginary = -95;
-    TEST_ASSERT_EQUAL(1, complex_mul(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(450, result.real);
-    TEST_ASSERT_EQUAL(3500, result.imaginary);
-
-    c1.real = 2;
-    c1.imaginary = 0;
-    
-    c2.real = -5;
-    c2.imaginary = 0;
-    TEST_ASSERT_EQUAL(1, complex_mul(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(-10, result.real);
-    TEST_ASSERT_EQUAL(0, result.imaginary);    
-
-    c1.real = 0;
-    c1.imaginary = 10;
-    
-    c2.real = 0;
-    c2.imaginary = 5;
-    TEST_ASSERT_EQUAL(1, complex_mul(&c1, &c2, &result));
-    TEST_ASSERT_EQUAL(-50, result.real);
-    TEST_ASSERT_EQUAL(0, result.imaginary);  
-
-}
-
-
-
-int test_main(void)
-{
-    /* Initiate the Unity Test Framework */
-    UNITY_BEGIN();
-
-    /* Run Test functions */
-    RUN_TEST(c_add);
-    RUN_TEST(c_sub);
-    RUN_TEST(c_mul);
-
-
-    /* Close the Unity Test Framework */
-    return UNITY_END();
+/* Run Test functions */
+  RUN_TEST(test_rs);
+  RUN_TEST(test_rp);
+  RUN_TEST(test_cp);
+  RUN_TEST(test_cs);
+  RUN_TEST(test_is);
+  RUN_TEST(test_ip);
+  RUN_TEST(test_rlcs);
+  RUN_TEST(test_rlcp);
+  RUN_TEST(test_rf);
+  /* Close the Unity Test Framework */
+  return UNITY_END();
 }
